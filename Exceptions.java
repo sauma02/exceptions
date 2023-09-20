690e9fd6f293f29d2e1d7489cb994b3b140281cd
saumaIqor <josedavids123@live.com>
2023-09-20 17:51:32 -0500

    Exceptions

---
+++ Exceptions.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exceptions;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exceptions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Exceptions");
+    }
+}
